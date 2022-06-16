ae6980a0ef2f8bc73a8c86ddcc61f2d19986d126
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2022-06-17 00:45:02 +0700

    add service to send email

--- src/main/java/com/pukul6/api/configuration/BeanConfiguration.java
+++ src/main/java/com/pukul6/api/configuration/BeanConfiguration.java
@@ -1,6 +1,7 @@
 package com.pukul6.api.configuration;
 
 import com.pukul6.api.utilities.MySmtpConfig;
+import dev.ditsche.mailo.config.MailoConfig;
 import dev.ditsche.mailo.provider.MailProvider;
 import dev.ditsche.mailo.provider.SmtpMailProvider;
 import lombok.RequiredArgsConstructor;
@@ -11,16 +12,38 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @RequiredArgsConstructor
 public class BeanConfiguration {
-	@Value(value = "${spring.}")
+	@Value(value = "${spring.email.username}")
+	private String username;
+	
+	@Value(value = "${spring.email.password}")
+	private String password;
+	
+	@Value(value = "${spring.email.host}")
+	private String host;
+	
+	@Value(value = "${spring.email.port}")
+	private Integer port;
+	
+	@Value(value = "${spring.email.mail.io.app.id}")
+	private String appId;
+	
+	@Value(value = "${spring.email.mail.io.app.secret}")
+	private String appSecret;
+	
+	
 	@Bean
 	public MailProvider mailProvider() {
+		var mailo = MailoConfig.get();
+		mailo.setMjmlAppId(this.appId);
+		mailo.setMjmlAppSecret(this.appSecret);
+		
 		var config = MySmtpConfig.builder()
-//				.username()
-//				.password()
-//				.host()
-//				.port()
+				.username(this.username)
+				.password(this.password)
+				.host(this.host)
+				.port(this.port)
 				.ssl(false)
 				.build();
-		return new SmtpMailProvider();
+		return new SmtpMailProvider(config);
 	}
 }
--- src/main/java/com/pukul6/api/utilities/MySmtpConfig.java
+++ src/main/java/com/pukul6/api/utilities/MySmtpConfig.java
@@ -1,15 +1,19 @@
 package com.pukul6.api.utilities;
 
 import dev.ditsche.mailo.config.SmtpConfig;
-import lombok.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.stereotype.Component;
 
 @Getter
 @Setter
-@NoArgsConstructor
-@AllArgsConstructor
 @Component
 public class MySmtpConfig extends SmtpConfig {
+	public MySmtpConfig() {
+		super();
+	}
+	
 	@Builder
 	public MySmtpConfig(String username, String password, String host, int port, boolean ssl) {
 		super.setUsername(username);
